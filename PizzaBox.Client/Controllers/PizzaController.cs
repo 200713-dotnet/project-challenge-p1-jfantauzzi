d91dad4cd112d5d7af6c08ea5b903065986c9b8f
jacob fantauzzi <jfantauzzi90@gmail.com>
2020-08-10 21:09:07 -0600

    Tidy Up. Starting to add edit functionality.

--- PizzaBox.Client/Controllers/PizzaController.cs
+++ PizzaBox.Client/Controllers/PizzaController.cs
@@ -17,48 +17,47 @@ namespace PizzaBox.Client.Controllers
       _db = dbContext;
     }
 
+    public IActionResult Edit()
+    {
+      ViewBag.PizzaList = _db.Pizzas.ToList();
+      //how do i input id? another action calls this one
+      return View( "Edit", _db.Pizzas.ToList() );
+      //return _db.Pizzas.SingleOrDefault(p => p.PizzaId == id);
+    }
+
     //get all pizzas
     [HttpGet()]
     public IActionResult Get()
     {
       ViewBag.PizzaList = _db.Pizzas.ToList();
-      
+
       return View("Home", _db.Pizzas.ToList());
     }
 
-    /* [HttpGet()]
-    public IActionResult */
-
-    //get specific pizza
-    [HttpGet("{id}")]
-    public IActionResult Edit(int id)
+    /* [HttpPost]
+    public IActionResult PizzaToEdit(List<Pizza> _db.Pizzas.ToList())
     {
-      //how do i input id? another action calls this one
-      return View("Edit", _db.Pizzas.SingleOrDefault(p => p.PizzaId == id));
-      //return _db.Pizzas.SingleOrDefault(p => p.PizzaId == id);
-    }
 
-    
-   /* [HttpDelete()]
-    public IActionResult Delete()
-    {
-      var db = new Repository();
-      ViewBag.PizzaList = _db.Pizzas.ToList();
-      if(ViewBag.PizzaList.rows.Any())
-      {
-        ViewBag.PizzaList.rows.Revomeat(ViewBag.PizzaList.rows.Count-1);
-      }
-
-
-
-      return View("/pizza/get", _db.Pizzas.ToList());
     } */
 
-  /* [HttpPost]
-  public IActionResult DeletePizza()
-  {
+  
+    //get specific pizza
 
-  } */
+
+
+    /* [HttpDelete()]
+     public IActionResult Delete()
+     {
+       var db = new Repository();
+       ViewBag.PizzaList = _db.Pizzas.ToList();
+       if(ViewBag.PizzaList.rows.Any())
+       {
+         ViewBag.PizzaList.rows.Revomeat(ViewBag.PizzaList.rows.Count-1);
+       }
+
+       return View("/pizza/get", _db.Pizzas.ToList());
+     } */
+
 
 
   }
